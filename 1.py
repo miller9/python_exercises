ae372266b42427da796b8ea5817bc81255ca25ac
MillerC <macarrenob@poligran.edu.co>
2018-02-23 17:42:25 -0500

    python_ex

---
+++ 1.py
@@ -0,0 +1,12 @@
+def max (a,b):
+	if (a>b):
+		return a
+	else:
+		return b
+
+z=max(18,21)
+w=str(z)
+
+print ("This function returns the max value between 2 arguments --> 18 & 21")
+print ("The max value is: " + w)
+#print (w)

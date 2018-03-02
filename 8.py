e6b8389fda9d0598c2a0dae675567b04b383ef7b
MillerC <millerpoli9@gmail.com>
2018-03-02 12:17:28 -0500

    function that recognizes palindromes

---
+++ 8.py
@@ -0,0 +1,38 @@
+def is_palindrome():
+	print ("""
+	Define a function is_palindrome() that recognizes palindromes (i.e. words that look the same written backwards).
+	For example, is_palindrome( "radar" ) should return True.	
+	""")
+	print ("---")
+	str1=str(input("Please enter the string to verify if its palindrome or not: "))
+	str2=""
+	subs=-1
+	i=0
+	for index, character in enumerate(str1):
+		str2 += str1[len(str1)+subs]
+		subs-=1
+
+	if str2[index] == str1[len(str1)+subs]:
+		print (True, "The word is palindrome!")
+	else:
+		print (False, "The word is not a palindrome!")
+	print ("---")
+	print ()
+"""
+Palindrome Examples: --> Dont use special characters:
+Alí tomó tila.
+Allí ves Sevilla.
+Aman a Panamá.
+A Mercedes ése de crema.
+Amad a la dama.
+A mamá Roma le aviva el amor a papá y a papá Roma le aviva el amor a mamá.
+Amigo no gima.
+A mí me mima.
+"""
+
+is_palindrome()
+
+
+
+
+

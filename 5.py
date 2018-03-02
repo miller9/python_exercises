d4b9efc5d49c33d18e73571abb5acf1e9c527126
MillerC <millerpoli9@gmail.com>
2018-03-02 08:48:47 -0500

    -rövarspråket- ==> Swedish for -robber's language- corrected

--- 5.py
+++ 5.py
@@ -4,25 +4,23 @@
 		str2=str2+str1[i]"""
 
 def translate():
-	print ()
-	"""
+	print (	"""
 Write a function translate() that will translate a text into "rövarspråket" (Swedish for "robber's language"). 
 That is, double every consonant and place an occurrence of "o" in between. 
 For example, translate("this is fun") should return the string "tothohisos isos fofunon".
-"""
+""")
+	print ()
 	print ("---")
 	str1=str(input("Please enter the string to be translated: "))
 	str2=" "
-	i=0
 	#vowel_list = set(('a','e','i','o','u'))
 	consonant_list=['b','c','d','f','g','h','j','k','l','m','n','ñ','p','q','r','s','t','v','w','x','y','z']	
 
-	for indice, carachter in enumerate(str1):
-		if str1[indice] in consonant_list:
-			str2+=(str1[i]+'o'+str1[i])
-	#		str2+='o'
-	#		str2+=str1[i]
-			i=i+1
+	for index, character in enumerate(str1):
+		if str1[index] in consonant_list:
+			str2+=(str1[index]+'o'+str1[index])
+		else: 
+			str2+=str1[index]
 	print ()	
 	print ("Our new 'rövarspråket' String is: "+str2)
 	print ("---")

45f496998fb3287182cee19b1f9752e0fc2a24b8
MillerC <millerpoli9@gmail.com>
2018-03-08 18:02:58 -0500

    histogram()

---
+++ 12.py
@@ -0,0 +1,18 @@
+print ("""
+	12. Define a procedure histogram() that takes a list of integers and prints a histogram to the screen.
+	For example, histogram( [ 4, 9, 7 ] ) should print the following:
+		****
+		*********
+		*******
+	""")
+
+def histogram(l):
+	for value in l:
+		print ("*"*value)
+
+int_list=[4,9,7]
+print ("The histogram of the list:",int_list,"is:")
+histogram(int_list)
+print ()
+
+

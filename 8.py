dfc4aea78bc704727f69bc0c0728b152dfc3a912
MillerC <millerpoli9@gmail.com>
2018-03-08 17:58:28 -0500

    max(a,b)

--- 8.py
+++ 8.py
@@ -1,28 +1,28 @@
-def is_palindrome():
-	print ("""
-	8. Define a function is_palindrome() that recognizes palindromes (i.e. words that look the same written backwards).
+print ("""
+	8. Define a function is_palindrome() that recognizes palindromes 
+	(i.e. words that look the same written backwards).
 	For example, is_palindrome( "radar" ) should return True.	
 	""")
+
+def is_palindrome():
+
 	print ("---")
 	str1=str(input("Please enter the string to verify if its palindrome or not: "))
 	str2=""
 	subs=-1
 	i=0
-	a=False
 	for index, character in enumerate(str1):
-		str2 += str1[len(str1)+subs]
+		str2+=str1[len(str1)+subs]
 		subs-=1
-		if str2[index] == str1[len(str1)+subs]:
-			a=True
-			# print (True, "The word is palindrome!")
-		else:
-			a=False
-			# print (False, "The word is not a palindrome!")
-	print ("is palindrome?",a)
+
+	if str2[index] == str1[len(str1)+subs]:
+		print (True, "--> The word is palindrome!")
+	else:
+		print (False, "--> The word is not a palindrome!")
 	print ("---")
 	print ()
 """
-Palindrome Examples: --> Do not use special characters:
+Palindrome Examples: --> Dont use special characters:
 Alí tomó tila.
 Allí ves Sevilla.
 Aman a Panamá.
